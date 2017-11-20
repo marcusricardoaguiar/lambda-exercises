a04d86cac289b2d08470f3fbebb0b0d1cf648833
masantos <masantos@fitec.org.br>
2017-11-20 09:26:01 -0200

    Ajuste na função para evitar utilizar o Optional. Kata 6.

--- src/main/java/katas/Kata6.java
+++ src/main/java/katas/Kata6.java
@@ -1,9 +1,6 @@
 package katas;
 
 import java.util.List;
-import java.util.Optional;
-
-import model.BoxArt;
 import model.Movie;
 import util.BoxArtUtil;
 import util.DataUtil;
@@ -16,11 +13,10 @@ import util.DataUtil;
 public class Kata6 {
     public static String execute() {
         List<Movie> movies = DataUtil.getMovies();
-        Optional<BoxArt> boxArt = movies.stream()
+        return movies.stream()
         	.flatMap(movie -> movie.getBoxarts().stream())
-        	.reduce(BoxArtUtil::largest);
-        
-        if(boxArt.isPresent()) return boxArt.get().getUrl();
-        else return "";
+        	.reduce(BoxArtUtil::largest)
+        	.map(boxArt -> boxArt.getUrl())
+        	.orElse("");
     }
 }
