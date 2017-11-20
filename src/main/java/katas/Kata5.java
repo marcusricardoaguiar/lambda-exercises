cfad02b95e58b2e8557a2c1aef031fba37868d8b
masantos <masantos@fitec.org.br>
2017-11-20 08:47:10 -0200

    Ajuste na função para evitar utilizar o Optional. Kata 5.

--- src/main/java/katas/Kata5.java
+++ src/main/java/katas/Kata5.java
@@ -1,8 +1,6 @@
 package katas;
 
 import java.util.List;
-import java.util.Optional;
-
 import model.Movie;
 import util.DataUtil;
 
@@ -12,13 +10,11 @@ import util.DataUtil;
     Output: Double
 */
 public class Kata5 {
-    public static Double execute() {
+	public static Double execute() {
         List<Movie> movies = DataUtil.getMovies();
-        Optional<Double> result = movies.stream()
+        return movies.stream()
             	.map(Movie::getRating)
-            	.reduce(Double::max);
-        
-        if(result.isPresent()) return result.get();
-        else return 0.0;
+            	.reduce(Double::max)
+            	.orElse(0.0);
     }
 }
